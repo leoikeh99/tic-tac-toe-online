952aabc57d4c34ac1e5f47a297d7b7320d620d63
Ikeh Leonard <leoikeh99@gmail.com>
2022-08-23 14:56:03 +0100

    Fixed directory problems

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    console.log(__dirname);
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(path.resolve(), "client", "build", "index.html"));
   });
 }
 
