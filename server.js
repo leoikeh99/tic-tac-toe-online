b2b2de79cd3e83eb34887a984069fd13fd8bf0b9
Ikeh Leonard <leoikeh99@gmail.com>
2022-08-23 14:13:54 +0100

    Prepare to deploy

--- server.js
+++ server.js
@@ -18,9 +18,10 @@ dotenv.config();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(path.dirname, "client", "build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
